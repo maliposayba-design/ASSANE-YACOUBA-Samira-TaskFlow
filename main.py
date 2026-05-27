efd3a413b05114d00781194a950c6d713fa9418f
maliposayba-design <samiraassaneyacouba11@gmail.com>
2026-05-27 19:24:09 +0000

    feat(affichage): menus colores avec codes ANSI

--- main.py
+++ main.py
@@ -5,3 +5,13 @@ conn = obtenir_connexion()
 if conn:
     print("Test réussi !")
     fermer_connexion(conn)
+    from affichage import *
+
+vider_ecran()
+
+afficher_succes("Connexion réussie")
+afficher_info("Bienvenue dans TaskFlow")
+afficher_alerte("Attention aux tâches urgentes")
+afficher_erreur("Exemple d'erreur")
+
+afficher_menu_principal("Samira")
